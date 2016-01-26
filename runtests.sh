e7cf1b1624124c21d41a622759dea6788b0a8ced
Kirill Klenov <horneds@gmail.com>
2016-01-26 19:19:13 +0300

    Update tests

--- runtests.sh
+++ runtests.sh
@@ -1,24 +1,63 @@
-#!/bin/sh
+#!/bin/bash
+
+IMAGES=(
+    "horneds/stouts-ubuntu14.04"
+    "horneds/stouts-debian8"
+)
+
+TESTS=(
+    "ansible-playbook -c local --syntax-check test.yml"
+    "ansible-playbook -c local test.yml"
+)
 
 APPDIR=/var/tests
 CURDIR=`pwd`
-IMAGE=horneds/ubuntu14.04-ansible
+ARGS=("$@")
 
-RUNNER=`docker run -v $CURDIR:$APPDIR -w $APPDIR -t -i -d horneds/ubuntu14.04-ansible bash`
+docker info || exit 1
 
 assert () {
-    docker exec -it $RUNNER $1 || ( echo ${2-'Test is failed'} && exit 1 )
+    echo "ASSERT: $1"
+    execute "$@" || ( echo ${2-'Test is failed'} && exit 1 )
+    echo "SUCCESS"
 }
 
-{
-    assert "ansible-playbook -c local --syntax-check test.yml"          &&
-    assert "ansible-playbook -c local test.yml"                         &&
-    assert "ansible-playbook -c local test.yml" | grep changed=0
-
-} || {
-
-    echo "Tests are failed"
-
+execute () {
+    docker exec -it $RUNNER $1
+    return $?
 }
 
-docker stop $RUNNER
+suite () {
+
+    echo "================="
+    echo "RUN IMAGE: $1"
+    echo "================="
+
+    for TEST in "${TESTS[@]}"; do
+        assert "$TEST" || ( echo "FAILED" && exit 1 )
+    done
+
+    echo
+    
+}
+
+
+for IMAGE in "${IMAGES[@]}"
+do
+
+    RUNNER=`docker run -v $CURDIR:$APPDIR -w $APPDIR -dit $IMAGE bash`
+
+    suite $IMAGE || {
+        echo "Tests are failed $IMAGE"
+        docker stop $RUNNER
+        exit 1
+    }
+
+    if [[ "$1" = "shell" ]]; then
+        echo "Run shell"
+        execute /bin/bash
+    fi
+
+    docker stop $RUNNER
+
+done
